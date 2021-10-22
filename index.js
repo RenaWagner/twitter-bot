4bae50c6233b319656c2b859d7c89f795132ac07
RenaWagner <rena.suzuki.wagner@gmail.com>
2021-10-22 19:32:27 +0200

    basic structure done

--- index.js
+++ index.js
@@ -3,37 +3,41 @@ const { TwitterClient } = require("twitter-api-client");
 const axios = require("axios");
 
 const twitterClient = new TwitterClient({
-  apiKey: process.env.TWITTER_API_KEY,
-  apiSecret: process.env.TWITTER_API_SECRET,
-  accessToken: process.env.TWITTER_ACCESS_TOKEN,
-  accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  // apiKey: process.env.TWITTER_API_KEY,
+  // apiSecret: process.env.TWITTER_API_SECRET,
+  // accessToken: process.env.TWITTER_ACCESS_TOKEN,
+  // accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  apiKey: "3aLNgJXIyJacMqbKKpj8zVk8o",
+  apiSecret: "I4rgfbcy4q4tuhUMINDyZXYgBQHklI5PUDIW7pkoDkBljIGdb6",
+  accessToken: "1451559947808952327-xPD6n4zyWTVTRaG4yqooxitC9h3sqi",
+  accessTokenSecret: "8gDQCjToITlqQxo0REqt8ks04Xam8FTfaCxZtvjUEN4LN",
 });
 
-module.exports = twitterClient;
+let randomId = Math.floor(Math.random() * 29) + 1;
 
 axios
-  .get("http://history.muffinlabs.com/date")
+  .get("https://twitter-chat-bot-be.herokuapp.com/quotes")
   .then((response) => {
-    const data = response.data.data ? response.data.data : {};
-    let tweet;
-    if (data.Events && data.Events.length) {
-      //tweet the first event in the array
-      tweet = "Year " + data.Events[0].year + " - " + data.Events[0].text;
-    } else {
-      tweet = "Nothing happened today :)";
-    }
-
-    //Send the tweet
-    twitterClient.tweets
-      .statusesUpdate({
-        status: tweet,
-      })
-      .then((response) => {
-        console.log("Tweeted!", response);
-      })
-      .catch((err) => {
-        console.error(err);
+    const data = response.data ? response.data : {};
+    if (data) {
+      const quote = data.find((item) => {
+        return item.id === randomId;
       });
+      tweet = `Tweet of this hour: ${quote.quote}`;
+    } else {
+      tweet = "You go girl! You can do this!";
+    }
+    //Send the tweet
+    // twitterClient.tweets
+    //   .statusesUpdate({
+    //     status: tweet,
+    //   })
+    //   .then((response) => {
+    //     console.log("Tweeted!", response);
+    //   })
+    //   .catch((err) => {
+    //     console.error(err);
+    //   });
   })
   .catch((err) => {
     console.error(err);
