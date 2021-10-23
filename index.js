f8895c6f8c7ef0a1ca5847a0967cc5a1101a7dd8
RenaWagner <rena.suzuki.wagner@gmail.com>
2021-10-23 09:28:11 +0200

    Updated the tweet methods

--- index.js
+++ index.js
@@ -3,14 +3,10 @@ const { TwitterClient } = require("twitter-api-client");
 const axios = require("axios");
 
 const twitterClient = new TwitterClient({
-  // apiKey: process.env.TWITTER_API_KEY,
-  // apiSecret: process.env.TWITTER_API_SECRET,
-  // accessToken: process.env.TWITTER_ACCESS_TOKEN,
-  // accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-  apiKey: "3aLNgJXIyJacMqbKKpj8zVk8o",
-  apiSecret: "I4rgfbcy4q4tuhUMINDyZXYgBQHklI5PUDIW7pkoDkBljIGdb6",
-  accessToken: "1451559947808952327-xPD6n4zyWTVTRaG4yqooxitC9h3sqi",
-  accessTokenSecret: "8gDQCjToITlqQxo0REqt8ks04Xam8FTfaCxZtvjUEN4LN",
+  apiKey: process.env.TWITTER_API_KEY,
+  apiSecret: process.env.TWITTER_API_SECRET,
+  accessToken: process.env.TWITTER_ACCESS_TOKEN,
+  accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
 let randomId = Math.floor(Math.random() * 29) + 1;
@@ -23,21 +19,22 @@ axios
       const quote = data.find((item) => {
         return item.id === randomId;
       });
-      tweet = `Tweet of this hour: ${quote.quote}`;
+      console.log("quote", quote);
+      tweet = `Tweet for you! "${quote.quote}"`;
     } else {
       tweet = "You go girl! You can do this!";
     }
     //Send the tweet
-    // twitterClient.tweets
-    //   .statusesUpdate({
-    //     status: tweet,
-    //   })
-    //   .then((response) => {
-    //     console.log("Tweeted!", response);
-    //   })
-    //   .catch((err) => {
-    //     console.error(err);
-    //   });
+    twitterClient.tweets
+      .statusesUpdate({
+        status: tweet,
+      })
+      .then((response) => {
+        console.log("Tweeted!", response);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   })
   .catch((err) => {
     console.error(err);
--- retweet.js
+++ retweet.js
@@ -2,14 +2,10 @@ require("dotenv").config();
 const { TwitterClient } = require("twitter-api-client");
 
 const twitterClient = new TwitterClient({
-  // apiKey: process.env.TWITTER_API_KEY,
-  // apiSecret: process.env.TWITTER_API_SECRET,
-  // accessToken: process.env.TWITTER_ACCESS_TOKEN,
-  // accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-  apiKey: "3aLNgJXIyJacMqbKKpj8zVk8o",
-  apiSecret: "I4rgfbcy4q4tuhUMINDyZXYgBQHklI5PUDIW7pkoDkBljIGdb6",
-  accessToken: "1451559947808952327-xPD6n4zyWTVTRaG4yqooxitC9h3sqi",
-  accessTokenSecret: "8gDQCjToITlqQxo0REqt8ks04Xam8FTfaCxZtvjUEN4LN",
+  apiKey: process.env.TWITTER_API_KEY,
+  apiSecret: process.env.TWITTER_API_SECRET,
+  accessToken: process.env.TWITTER_ACCESS_TOKEN,
+  accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
 twitterClient.tweets
