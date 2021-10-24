aedb97f381bf760de2a54bd8c69362b497584b4c
RenaWagner <rena.suzuki.wagner@gmail.com>
2021-10-24 14:04:32 +0200

    Updates

--- index.js
+++ index.js
@@ -101,10 +101,13 @@ const tweetData = async () => {
 };
 
 tweetData();
-console.log("TWITTER_API_KEY", TWITTER_API_KEY);
-console.log("process.env.TWITTER_API_SECRET", TWITTER_API_SECRET);
-console.log("process.env.TWITTER_ACCESS_TOKEN", TWITTER_ACCESS_TOKEN);
+console.log("TWITTER_API_KEY", process.env.TWITTER_API_KEY);
+console.log("process.env.TWITTER_API_SECRET", process.env.TWITTER_API_SECRET);
+console.log(
+  "process.env.TWITTER_ACCESS_TOKEN",
+  process.env.TWITTER_ACCESS_TOKEN
+);
 console.log(
   "process.env.TWITTER_ACCESS_TOKEN_SECRET",
-  TWITTER_ACCESS_TOKEN_SECRET
+  process.env.TWITTER_ACCESS_TOKEN_SECRET
 );
