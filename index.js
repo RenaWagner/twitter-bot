91dec98d48bc9e6638ed1241ae44384e9955faeb
RenaWagner <rena.suzuki.wagner@gmail.com>
2021-10-24 13:07:45 +0200

    update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
-let randomId = Math.floor(Math.random() * 23) + 1;
+let randomId = Math.floor(Math.random() * 25);
 
 var download = async function (uri, filename, quote) {
   let result;
